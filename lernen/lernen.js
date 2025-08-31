b1b1ae527dff146494993ab43626f864c5c49a05
Dexarion <173498033+Dexarion@users.noreply.github.com>
2025-08-31 20:18:28 +0200

    Initial commit

---
+++ lernen/lernen.js
@@ -0,0 +1,48 @@
+let zuhause= "home";
+const lernen = "schule";
+let baum= "arhorn";
+baum= "buche";
+const step1 = "collect pants";
+const stap2 = "?";
+const stap3 = "Profit";
+console.log(zuhause)
+console.log("zuhause"+"55");
+const alter= "23";
+const ricci= "21";
+const newalter= alter+ricci;
+const reandomnummer= "5";
+const neuenummer= "O:5/100";
+const richtig=true;
+console.log(richtig);
+console.log(!false);
+console.log(10===10);
+console.log(1!==10);
+console.log(1<90);
+console.log(1<=3);
+console.log("apfel"==="apfel");
+let batteryon = true;
+let switchon = true;
+console.log(batteryon && switchon)
+console.log(batteryon || switchon)
+if (true){console.log("hello!");}
+else {console.log(2)}
+function show() { document. getElementById("secret").innerHTML;}
+let hour=20;
+if(hour<12){
+console.log("Good morning");
+} else if (hour<21) {console.log("Good evening");
+} else if (hour <17) {
+console.log("Good afternoon");
+} else  {console.log("Good night");}
+const element = document.getElementById("messages");
+console.log(element.innerHTML);
+element.innerHTML= "No Messages";
+function publish() {const el = 
+document.querySelector("p.prompt");
+el.innerHTML="Website published";
+}
+function publish() {const el = 
+document.querySelector("#prompt");
+el.innerHTML="Website published";
+}
+const el= document.querySelectorAll(".movie,.tech");
